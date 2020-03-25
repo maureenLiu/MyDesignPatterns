9c36c48602a4f895921115c63d1193e79e5c1967
Maureen <1437867946@qq.com>
2020-03-25 17:23:40 +0800

    单例模式实现-枚举

---
+++ src/main/java/com/maureen/dp/singleton/Mgr08.java
@@ -0,0 +1,18 @@
+package com.maureen.dp.singleton;
+
+/**
+ * 枚举的方式
+ * 不仅可以解决线程同步问题，还可以防止反序列化
+ */
+
+public enum Mgr08 {
+    INSTANCE;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                System.out.println(Mgr08.INSTANCE.hashCode());
+            }).start();
+        }
+    }
+}
