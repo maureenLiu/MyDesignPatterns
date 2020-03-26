8ff4edffe05ff328f500d0a4cb13003cc473310d
Maureen <1437867946@qq.com>
2020-03-26 18:30:53 +0800

    代理模式

---
+++ src/main/java/com/maureen/dp/proxy/v4/Tank.java
@@ -0,0 +1,51 @@
+package com.maureen.dp.proxy.v4;
+
+import java.util.Random;
+
+/**
+ * 问题：我想记录坦克的移动时间
+ * 最简单的方法：修改代码，记录时间
+ * 问题2：如果无法修改方法源码呢？
+ * 用继承？
+ * v04:使用代理
+ */
+
+public class Tank implements Movable {
+    /**
+     * 模拟坦克移动了一段时间
+     */
+    @Override
+    public void move() {
+        System.out.println("Tank moving claclacla...");
+        try {
+            Thread.sleep(new Random().nextInt(10000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new TankTimeProxy(new Tank()).move();
+    }
+}
+
+class TankTimeProxy implements Movable {
+    Tank tank;
+
+    public TankTimeProxy(Tank tank) {
+        this.tank = tank;
+    }
+
+    @Override
+    public void move() {
+        long start = System.currentTimeMillis();
+        tank.move();
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+}
+
+
+interface Movable {
+    void move();
+}
