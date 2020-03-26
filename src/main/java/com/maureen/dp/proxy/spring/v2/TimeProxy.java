cbb4841659807aca56afc6fec62327d4b9b76132
Maureen <1437867946@qq.com>
2020-03-26 18:02:54 +0800

    代理模式--动态代理：Spring AOP实现的注解配置写法

---
+++ src/main/java/com/maureen/dp/proxy/spring/v2/TimeProxy.java
@@ -0,0 +1,19 @@
+package com.maureen.dp.proxy.spring.v2;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class TimeProxy {
+    @Before("execution(void com.maureen.dp.proxy.spring.v2.Tank.move())")
+    public void before() {
+        System.out.println("method start..." + System.currentTimeMillis());
+    }
+
+    @After("execution(void com.maureen.dp.proxy.spring.v2.Tank.move())")
+    public void after() {
+        System.out.println("method stop..." + System.currentTimeMillis());
+    }
+
+}
