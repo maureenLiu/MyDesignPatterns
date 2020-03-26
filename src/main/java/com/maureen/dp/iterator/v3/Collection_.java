13f8bd4215831ff10b903dfddf08744c3be97a6b
Maureen <1437867946@qq.com>
2020-03-26 13:29:19 +0800

    Iterator迭代器模式-添加容器的共同接口，实现容器的替换

---
+++ src/main/java/com/maureen/dp/iterator/v3/Collection_.java
@@ -0,0 +1,7 @@
+package com.maureen.dp.iterator.v3;
+
+public interface Collection_ {
+    void add(Object o);
+
+    int size();
+}
---
+++ src/main/java/com/maureen/dp/iterator/v3/LinkedList_.java
@@ -0,0 +1,40 @@
+package com.maureen.dp.iterator.v3;
+
+/**
+ * 相比数组，这个容器不用考虑边界问题，可以动态扩展
+ */
+//底层使用链表实现
+public class LinkedList_ implements Collection_ {
+    Node head = null;
+    Node tail = null;
+
+    private int size = 0;
+
+    @Override
+    public void add(Object o) {
+        Node n = new Node(o);
+        n.next = null;
+
+        if (head == null) {
+            head = n;
+            tail = n;
+        }
+        tail.next = n;
+        tail = n;
+        size++;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private class Node {
+        private Object o;
+        Node next;
+
+        public Node(Object o) {
+            this.o = o;
+        }
+    }
+}
